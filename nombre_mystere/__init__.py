e70ede4eb29b1c453d3b4954a3f1241460c50402
Manoarijaona <manoa.mj@gmail.com>
2022-11-01 21:52:42 +0100

    Add level and name

--- nombre_mystere/__init__.py
+++ nombre_mystere/__init__.py
@@ -1,10 +1,25 @@
 import random
+name = input("Donne ton pseudo de gamer: ")
+print(f"Salut {name}, tu es plutôt un:\n")
+print("1- Noob")
+print("2- Petit joueur")
+print("3- Pro gamer")
+level = int(input())
 
-mystery_number = random.randint(1, 100)
+if level == 1:
+    mystery_number = random.randint(1, 20)
+    max_number = 20
+elif level == 2:
+    mystery_number = random.randint(1, 100)
+    max_number = 100
+elif level == 3:
+    mystery_number = random.randint(1, 1000)
+    max_number = 1000
+
 initial_number = 0
 score = 10
 while mystery_number != initial_number:
-    input_number = int(input("Entrez un nombre entre 1 et 100: "))
+    input_number = int(input(f"Entrez un nombre entre 1 et {max_number}: "))
     if mystery_number > input_number:
         print(f"Le nombre mystère est supérieur à {input_number}")
         initial_number = input_number
